5c7a8fa5d33b6ae72a401070b8c953a060b07b4f
Javier Arce <javierarce@gmail.com>
2012-11-06 22:33:34 +0100

    add paths

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var map, infowindow;
-
+map        = null,
+paths      = [],
+infowindow = null;
 
 CONFIG = {
+  dataURL:         "http://wsjgraphics.cartodb.com/api/v1/sql?q=SELECT%20ST_X(rs.the_geom)%20as%20rs_longitude,%20ST_Y(rs.the_geom)%20as%20rs_latitude,ST_X(hs.the_geom)%20as%20hs_longitude,%20ST_Y(hs.the_geom)%20as%20hs_latitude,ST_AsGeoJSON(sd.the_geom,4)%20as%20route,rs.relocating_school_bn,rs.host_bldg_id,directions_time,address_of_relocating_school,%20hs.host_bldg_name,rs.name_of_relocating_school%20FROM%20schools_directions%20as%20sd%20INNER%20JOIN%20relocating_schools%20as%20rs%20ON%20sd.relocating_school_bn=rs.relocating_school_bn%20INNER%20JOIN%20host_schools%20as%20hs%20ON%20hs.host_bldg_id=rs.host_bldg_id",
   relocatingURL:   "http://wsjgraphics.cartodb.com/api/v1/sql?q=SELECT%20ST_X(the_geom)%20as%20longitude,%20ST_Y(the_geom)%20as%20latitude,%20address_of_relocating_school,borough,grade_levels_that_are_relocating,host_bldg_id%20FROM%20relocating_schools",
   hostingURL:      "http://wsjgraphics.cartodb.com/api/v1/sql?q=SELECT%20ST_X(the_geom)%20as%20longitude,ST_Y(the_geom)%20as%20latitude,host_bldg_address,host_bldg_id,host_bldg_name,host_boro%20FROM%20host_schools",
   pathURL:         "http://wsjgraphics.cartodb.com/api/v1/sql?q=SELECT%20ST_AsGeoJSON(the_geom),relocating_school_bn,host_bldg_id,directions_time%20FROM%20schools_directions",
@@ -10,7 +12,9 @@ CONFIG = {
   relocatingIcon:  { path: google.maps.SymbolPath.CIRCLE, fillColor: "red",   fillOpacity: 0.8, scale: 5, strokeColor: "red",   strokeWeight: 0 },
 };
 
-function addMarker(map, type, data) {
+window.paths = paths;
+
+function addMarker(map, type, coordinates, data) {
 
   var icon = null;
 
@@ -20,21 +24,31 @@ function addMarker(map, type, data) {
     icon = CONFIG.relocatingIcon
   }
 
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(data.latitude, data.longitude),
+  return new google.maps.Marker({
+    position: new google.maps.LatLng(coordinates[0], coordinates[1]),
     icon: icon,
     map: map,
     type: type,
     data: data
   });
-
-
-  google.maps.event.addListener(marker, 'click', onMarkerClick);
-
 }
 
-function onMarkerClick(event) {
-  //console.log(this, event);
+
+function drawPath(route) {
+  var geo  = JSON.parse(route);
+  var path = new GeoJSON(geo, CONFIG.pathStyle);
+  path[0].setMap(map);
+  return path[0];
+}
+
+function selectPath(e) {
+  var that = this;
+
+  _.each(paths, function(p) {
+    if (p.data.host_bldg_id != that.data.host_bldg_id) p.path.setMap(null);
+    else p.path.setMap(map);
+  });
+
 
   if (this.type != "hosting") {
     if (this.data.address_of_relocating_school) {
@@ -44,34 +58,27 @@ function onMarkerClick(event) {
   }
 
   infowindow.setContent(content);
-  infowindow.setPosition(event.latLng);
+  infowindow.setPosition(e.latLng);
   infowindow.open();
-}
 
-function addPoints(url, type) {
-
-  $.ajax({ url: url, success: function(data) {
-
-    _.each(data.rows, function(d) {
-      addMarker(map, type, d);
-    });
-
-  }});
 
 }
-
 
 function draw() {
 
-
-  $.ajax({ url: CONFIG.pathURL, success: function(data) {
+  $.ajax({ url: CONFIG.dataURL, success: function(data) {
 
     var results = data.rows;
-
     _.each(results, function(p) {
-      var geo = JSON.parse(p.st_asgeojson);
-      var path = new GeoJSON(geo, CONFIG.pathStyle);
-      path[0].setMap(map);
+
+      var hosting    = addMarker( map, "hosting",    [p.hs_latitude, p.hs_longitude], p );
+      var relocating = addMarker( map, "relocating", [p.rs_latitude, p.rs_longitude], p );
+
+      paths.push({ data: p, hosting: hosting, relocating: relocating, path: drawPath(p.route) });
+
+      google.maps.event.addListener(relocating, 'click', selectPath);
+      google.maps.event.addListener(hosting,    'click', selectPath);
+
     });
 
   }});
@@ -89,10 +96,8 @@ function init() {
     zoom: 11,
   });
 
-  addPoints(CONFIG.relocatingURL, "relocating");
-  addPoints(CONFIG.hostingURL,    "hosting");
-
   draw();
+
   infowindow = new CartoDBInfowindow(map);
 }
 
