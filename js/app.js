57eff307e8b65d816730cbc8394b0bdc585a54c2
Javier Arce <javierarce@gmail.com>
2012-11-06 21:28:19 +0100

    Adds tyle

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-
-var map, infowindow, cartodb_gmaps1, cartodb_gmaps2;
+var map, infowindow;
+var pathStyle = {"strokeColor": "#FFFF00", "strokeWeight": 7, "strokeOpacity": 0.75 }
 
 function addMarker(map, type, lat, lng) {
 
@@ -68,35 +68,29 @@ function getLines() {
 
 function draw() {
 
-var geo = {
-  "type": "FeatureCollection",
-  "features": [{
-    "type": "Feature",
-    "properties": {
-      "st_astext": "MULTILINESTRING((-71.160281 42.258729,-71.160837 42.259113,-71.161144 42.25932))"
-    },
-    "geometry": {
-      "type": "MultiLineString",
-      "coordinates": [
-        [
-          [-71.160281, 42.258729],
-          [-71.160837, 42.259113],
-          [-71.161144, 42.25932]
+  var geo = {
+    "type": "FeatureCollection",
+    "features": [{
+      "type": "Feature",
+      "properties": {
+        "st_astext": "MULTILINESTRING((-71.160281 42.258729,-71.160837 42.259113,-71.161144 42.25932))"
+      },
+      "geometry": {
+        "type": "MultiLineString",
+        "coordinates": [
+          [
+            [-71.160281, 42.258729],
+            [-71.160837, 42.259113],
+            [-71.161144, 42.25932]
+          ]
         ]
-      ]
-    }
-  }]
-}
+      }
+    }]
+  }
 
-var path = new GeoJSON(geo, {
-  "strokeColor": "#FFFF00",
-  "strokeWeight": 7,
-  "strokeOpacity": 0.75
+  var path = new GeoJSON(geo, pathStyle);
 
-});
-
- path[0][0].setMap(map);
-
+  path[0][0].setMap(map);
 }
 
 
@@ -116,71 +110,6 @@ function init() {
   draw();
   infowindow = new CartoDBInfowindow(map);
 
-  /*cartodb_gmaps2 = new CartoDBLayer({
-    map: map,
-    user_name:"wsjgraphics",
-    table_name: 'relocating_schools',
-    query: "SELECT the_geom_webmercator, ST_X(the_geom) as longitude, ST_Y(the_geom) as latitude, address_of_relocating_school,borough,grade_levels_that_are_relocating,host_bldg_id FROM {{table_name}}",
-    //tile_style: "#{{table_name}}{marker-fill:#E25B5B}",
-    //interactivity: "cartodb_id",
-    opacity:1,
-    featureOver: function(ev, latlng, pos, data) {
-    map.setOptions({draggableCursor: 'pointer'});
-    },
-    featureOut: function() {
-    map.setOptions({draggableCursor: 'default'});
-    },
-    featureClick: function(ev, latlng, pos, data) {
-    //ev.stopPropagation ? ev.stopPropagation() : window.event.cancelBubble = true;
-
-    // Set infowindow content
-    infowindow.setContent(data);
-
-    // Set infowindow latlng
-    infowindow.setPosition(latlng);
-
-    // Show it!
-    infowindow.open();
-    },
-    auto_bound: false,
-    debug: true
-    });*/
-
-
-
-
-
-    /*google.maps.event.addListener(cartodb_gmaps2,"removed",function(){
-      if ('console' in self && 'log' in console) console.log("removed")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"added",function(){
-      if ('console' in self && 'log' in console) console.log("added")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"updated",function(){
-      if ('console' in self && 'log' in console) console.log("updated")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"shown",function(){
-      if ('console' in self && 'log' in console) console.log("shown")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"hidden",function(){
-      if ('console' in self && 'log' in console) console.log("hidden")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"tileerror",function(){
-      if ('console' in self && 'log' in console) console.log("tile - error")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"loading",function(){
-      if ('console' in self && 'log' in console) console.log("loading")
-      });
-
-      google.maps.event.addListener(cartodb_gmaps2,"loaded",function(){
-      if ('console' in self && 'log' in console) console.log("loaded")
-      });*/
 
 }
 
