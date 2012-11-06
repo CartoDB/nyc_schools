5144d8d5df158838d5c42a84418cb2e077f65a7c
Javier Arce <javierarce@gmail.com>
2012-11-06 20:36:15 +0100

    Adds test map

--- js/app.js
+++ js/app.js
@@ -1,24 +1,108 @@
-$(function() {
 
-var map = new google.maps.Map(document.getElementById('map'), {
-  center: new google.maps.LatLng(20, 0),
-  zoom: 3,
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  mapTypeControl: false
-});
+ var map, infowindow, cartodb_gmaps1, cartodb_gmaps2;
 
-var cartodb_gmapsv3 = new CartoDBLayer({
-  map: map,
-  user_name:'examples',
-  table_name: 'earthquakes',
-  query: "SELECT * FROM {{table_name}}",
-  layer_order: "top",
-  tile_style: "#{{table_name}}{marker-fill:red}",
-  interactivity: "cartodb_id, magnitude",
-  featureClick: function(feature, latlng, pos, data) {alert(feature)},
-  featureOut: function() {},
-  featureOver: function(feature, latlng, pos, data) {},
-  auto_bound: true
-});
+  function init() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(7.369722, 12.354722),
+      disableDefaultUI: false,
+      zoom: 3,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false
+    });
 
-});
+    infowindow = new CartoDBInfowindow(map);
+
+    wax_layer = wax.tilejson('http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp', function(tilejson) {
+
+      map.mapTypes.set('mb', new wax.g.connector(tilejson));
+      map.setMapTypeId('mb');
+
+      wax.g.interaction()
+        .map(map)
+        .tilejson(tilejson)
+        .on(wax.tooltip().parent(map.getDiv()).events());
+    });
+
+
+    // First cartodb layer, countries
+    cartodb_gmaps1 = new CartoDBLayer({
+      map: map,
+      user_name:'examples',
+      table_name: 'country_colors',
+      query: "SELECT * FROM {{table_name}}",
+      opacity: 0.2,
+      auto_bound: false,
+      debug: true
+    });
+
+
+    // Second cartodb layer, earthquakes
+    cartodb_gmaps2 = new CartoDBLayer({
+      map: map,
+      user_name:"examples",
+      table_name: 'earthquakes',
+      query: "SELECT cartodb_id,the_geom_webmercator,the_geom,magnitude FROM {{table_name}}",
+      tile_style: "#{{table_name}}{marker-fill:#E25B5B}",
+      interactivity: "cartodb_id, magnitude",
+      opacity:1,
+      featureOver: function(ev, latlng, pos, data) {
+        map.setOptions({draggableCursor: 'pointer'});
+      },
+      featureOut: function() {
+        map.setOptions({draggableCursor: 'default'});
+      },
+      featureClick: function(ev, latlng, pos, data) {
+        //ev.stopPropagation ? ev.stopPropagation() : window.event.cancelBubble = true;
+
+        // Set infowindow content
+        infowindow.setContent(data);
+
+        // Set infowindow latlng
+        infowindow.setPosition(latlng);
+
+        // Show it!
+        infowindow.open();
+      },
+      auto_bound: false,
+      debug: true
+    });
+
+
+
+
+
+    google.maps.event.addListener(cartodb_gmaps2,"removed",function(){
+      if ('console' in self && 'log' in console) console.log("removed")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"added",function(){
+      if ('console' in self && 'log' in console) console.log("added")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"updated",function(){
+      if ('console' in self && 'log' in console) console.log("updated")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"shown",function(){
+      if ('console' in self && 'log' in console) console.log("shown")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"hidden",function(){
+      if ('console' in self && 'log' in console) console.log("hidden")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"tileerror",function(){
+      if ('console' in self && 'log' in console) console.log("tile - error")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"loading",function(){
+      if ('console' in self && 'log' in console) console.log("loading")
+    });
+
+    google.maps.event.addListener(cartodb_gmaps2,"loaded",function(){
+      if ('console' in self && 'log' in console) console.log("loaded")
+    });
+
+  }
+
+
