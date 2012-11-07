7e9a7beb7b72a8a52b2d2939cd684317dec24cdc
Javier Arce <javierarce@gmail.com>
2012-11-08 00:20:25 +0100

    fixes

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@ paths      = [],
 infowindow = null;
 
 CONFIG = {
-  //dataURL: "http://wsjgraphics.cartodb.com/api/v1/sql?q=SELECT%20ST_X(rs.the_geom)%20as%20rs_longitude,%20ST_Y(rs.the_geom)%20as%20rs_latitude,ST_X(hs.the_geom)%20as%20hs_longitude,%20ST_Y(hs.the_geom)%20as%20hs_latitude,ST_AsGeoJSON(sd.the_geom,4)%20as%20route,rs.relocating_school_bn,rs.host_bldg_id,directions_time,address_of_relocating_school,%20hs.host_bldg_name,rs.grade_levels_that_are_relocating,rs.name_of_relocating_school%20FROM%20schools_directions%20as%20sd%20INNER%20JOIN%20relocating_schools%20as%20rs%20ON%20sd.relocating_school_bn=rs.relocating_school_bn%20INNER%20JOIN%20host_schools%20as%20hs%20ON%20hs.host_bldg_id=rs.host_bldg_id",
   dataURL: "http://wsjgraphics.cartodb.com/api/v1/sql?q=%20SELECT%20array_agg(ST_X(hs.the_geom)%7C%7C','%7C%7CST_Y(hs.the_geom))%20as%20host_coordinates,%20array_agg(rs.host_bldg_id)%20as%20host_bldg_ids,%20array_agg(rs.host_building_name)%20as%20host_building_names,%20array_agg(rs.host_building_address)%20as%20host_building_addresses,%20array_agg(grade_levels_that_are_relocating)%20as%20grades,%20ST_X(rs.the_geom)%7C%7C','%7C%7CST_Y(rs.the_geom)%20as%20coordinates_relocating_school,%20rs.name_of_relocating_school,%20addressofrelocatingschool,%20relocating_school_bn%20FROM%20relocating_lines%20as%20rs%20inner%20join%20hosting_schools%20as%20hs%20on%20rs.host_bldg_id=hs.host_bldg_id%20GROUP%20BY%20rs.the_geom,name_of_relocating_school,addressofrelocatingschool,relocating_school_bn",
   mapStyle: [ { stylers: [ { saturation: -65 }, { gamma: 1.52 } ] },{ featureType: "administrative", stylers: [ { saturation: -95 }, { gamma: 2.26 } ] },{ featureType: "water", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "administrative.locality", stylers: [ { visibility: "off" } ] },{ featureType: "road", stylers: [ { visibility: "simplified" }, { saturation: -99 }, { gamma: 2.22 } ] },{ featureType: "poi", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "road.arterial", stylers: [ { visibility: "off" } ] },{ featureType: "road.local", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "transit", stylers: [ { visibility: "off" } ] },{ featureType: "road", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "poi", stylers: [ { saturation: -55 } ] } ],
-
   pathStyle:       {"strokeColor": "#333", "strokeWeight": 2, "strokeOpacity": .8 },
   hostingIcon:     { path: google.maps.SymbolPath.CIRCLE, fillColor: "black", fillOpacity: 0.8, scale: 5, strokeColor: "black", strokeWeight: 0 },
   relocatingIcon:  { path: google.maps.SymbolPath.CIRCLE, fillColor: "red",   fillOpacity: 0.8, scale: 5, strokeColor: "black",   strokeWeight: 1 },
@@ -74,7 +72,6 @@ function selectPath(e) {
       }
     });
 
-console.log(this.data);
     content = "<p><strong>Host Blg Name</strong><br />" +
       this.data.host_building_names[this.i]+"</p>" +
       "<p><strong>Name of relocating school</strong><br />" +
@@ -93,7 +90,7 @@ console.log(this.data);
     content = "<p><strong>Name of relocating school</strong><br />" +
       this.data.name_of_relocating_school+"</p>" +
       "<p><strong>Grade levels that are relocating</strong><br />" +
-      this.data.grades_levels_that_are_relocating.join("<br />")+"</p>" +
+      this.data.grades.join("<br />")+"</p>" +
       "<p><strong>Host Blg Name</strong><br />" +
       this.data.host_building_names.join("<br />")+"</p>";
   }
@@ -112,28 +109,28 @@ function draw() {
     _.each(results, function(p) {
 
       var i = 0;
-
+      var relocating_coordinates = p.coordinates_relocating_school.split(",");
+      var rc = [parseFloat(relocating_coordinates[1], 10) , parseFloat(relocating_coordinates[0], 10)];
       _.each(p.host_coordinates, function(c) {
 
         if (c != null) {
 
           var hosting_coordinates    = c.split(",");
-          var relocating_coordinates = p.coordinates_relocating_school.split(",");
 
           var hc = [parseFloat(hosting_coordinates[1], 10), parseFloat(hosting_coordinates[0], 10)];
-          var rc = [parseFloat(relocating_coordinates[1], 10) , parseFloat(relocating_coordinates[0], 10)];
 
           var hosting    = addMarker( map, "hosting",    hc, p, i );
-          var relocating = addMarker( map, "relocating", rc, p, 0);
 
           paths.push({ data: p, path: drawPath(hc, rc) });
 
-          google.maps.event.addListener(relocating, 'click', selectPath);
           google.maps.event.addListener(hosting,    'click', selectPath);
 
           i++;
-
         }
+
+        var relocating = addMarker( map, "relocating", rc, p, 0);
+        google.maps.event.addListener(relocating, 'click', selectPath);
+
       });
 
     });
